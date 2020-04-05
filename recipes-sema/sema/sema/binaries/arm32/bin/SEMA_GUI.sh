28be54a2bf6f9bfb751b16e81a4e043eab66b824
Dinesh kumar <dineshkumar.varadarajan@adlinktech.com>
2020-04-05 11:36:32 +0530

    Sema4.0 Branch

--- recipes-sema/sema/sema/binaries/arm32/bin/SEMA_GUI.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PATH=/usr/local/SEMA/semagui:$PATH
-export LD_LIBRARY_PATH=/usr/local/SEMA/lib:/usr/local/SEMA/semagui
-export QT_X11_NO_MITSHM=1
-chmod 755 /usr/local/SEMA/semagui/SEMAGUI
-/usr/local/SEMA/semagui/SEMAGUI > /dev/null 2>&1
